4319fe56da6a1340bd0f611e1d840da6b2dc3fd8
tequila <tequila.gogo55@gmail.com>
2019-11-23 07:34:38 +0900

    test commit

--- invest.bat
+++ invest.bat
@@ -1,2 +1,3 @@
 mkdir out_invest
 for %%a in (testdata\*.txt) do python jsons_to_all_csv.py %%a out_invest\%%~na.txt
+pause
--- test.bat
+++ test.bat
@@ -1,2 +1,3 @@
 mkdir out_test
 for %%a in (testdata\*.txt) do python jsons_to_koa_csv.py %%a out_test\%%~na.csv
+pause
