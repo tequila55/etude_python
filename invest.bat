1846384016bac73850983edf435f907b7063320f
tequila <tequila.gogo55@gmail.com>
2019-11-23 23:35:22 +0900

    batch fix

--- invest.bat
+++ invest.bat
@@ -1,3 +1,3 @@
 mkdir out_invest
-for %%a in (testdata\*.txt) do python jsons_to_all_csv.py %%a out_invest\%%~na.txt
+for %%a in (testdata\*.json) do python jsons_to_all_csv.py %%a out_invest\%%~na.txt
 pause
